426cb6572c063905fab45ec705760d40e988b53f
joaocondereis <131829105+joaocondereis@users.noreply.github.com>
2023-09-21 20:31:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,16 +13,25 @@ recognition.onresult = function(event){
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
     speak();
+    if (Content == "tire minha selfie") {
+        console.log("tirando selfie --- ");
+        speak();
+    }
 }
 
 function speak(){
     var synth = window.speechSynthesis;
     
-    speak_data = document.getElementById("textbox").value;
-    var utterThis = new speechSynthesisUttarance(speak_data);
+    speak_data = "Tirando sua selfie em 5 segundos";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 5000)
 
 }
 
@@ -34,3 +43,17 @@ Webcam.set({
     image_format : 'jpeg',
     jpeg_quality:90
 });
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML =
+        '<img id="selfie_image" src="' + data_uri + '"/>';
+    })
+}
+
+   function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+   }
